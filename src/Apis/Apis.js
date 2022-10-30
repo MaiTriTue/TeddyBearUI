e062c58a4c8e6726f5f97000acf3119253938027
Tri Tue Mai <HacTinhThan@gmail.com>
2022-10-30 20:12:33 +0700

    lan001

--- src/Apis/Apis.js
+++ src/Apis/Apis.js
@@ -21,5 +21,6 @@ export let endpoints = {
 };
 
 export default axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    // baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'https://teddybearbackend.herokuapp.com',
 });
